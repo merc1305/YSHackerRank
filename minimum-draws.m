4dc74052e2772c3619919f9ff4a6e84baa37db71
Yauheni Shauchenka <merc1305@gmail.com>
2014-12-23 12:22:34 +0300

    minimum-draws solution

---
+++ minimum-draws.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", n+1);
+    }
+    return 0;
+}
